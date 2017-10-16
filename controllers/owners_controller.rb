21619dbbf92921622516f703d61d470baa10dbfd
hdpuk86 <hayley.d.prior@gmail.com>
2017-10-16 14:18:37 +0100

    adds new owner form

--- controllers/owners_controller.rb
+++ controllers/owners_controller.rb
@@ -7,3 +7,13 @@ get '/owners' do
   @owners = Owner.all()
   erb(:"owners/index")
 end
+
+get '/owners/new' do
+  erb(:"owners/new")
+end
+
+post '/owners' do
+  owner = Owner.new(params)
+  owner.save()
+  redirect to('/owners')
+end
