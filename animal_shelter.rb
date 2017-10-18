cfbf6fb3c9b9560843d0e8de086bd6397e77552a
hdpuk86 <hayley.d.prior@gmail.com>
2017-10-18 11:41:13 +0100

    adds counts to homepage

--- animal_shelter.rb
+++ animal_shelter.rb
@@ -6,11 +6,9 @@ require_relative 'controllers/pets_controller'
 
 require_relative 'models/pet.rb'
 require_relative 'models/adoption.rb'
-require_relative 'models/owner.rb'
 
 get '/' do
   @newest = Pet.most_recent()
   @latest = Adoption.latest()
-  @owners = Owner.all()
   erb( :index )
 end
